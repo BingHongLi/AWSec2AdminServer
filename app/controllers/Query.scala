3582496fed422c0eedd74a9db1640327ec5b1ac3
BingHongLi <b97607065@gmail.com>
2015-11-04 23:32:52 +0800

    Write comments for Query.scala

--- app/controllers/Query.scala
+++ app/controllers/Query.scala
@@ -17,13 +17,13 @@ import play.api.data.Forms._
 class Query extends Controller {
 
   def getWPqueryEC2Info() = Action{
-
+//  此函數用來回傳queryEC2Info網頁，使用者尚未輸入表單時的頁面呈現
     Ok(views.html.queryEC2Info(play.twirl.api.Html("test")))
   }
 
-
+// 建立表單，所需使用的樣例類，並使用play套件映射，告知是什麼性質的表單
+// 用在queryEC2Info的postReturnEC2Info方法
   case class User(keyName:String,systemPassword:String)
-
   val userForm = play.api.data.Form(
     mapping(
       "keyName"->text,
@@ -31,16 +31,18 @@ class Query extends Controller {
     )(User.apply)(User.unapply)
   )
 
+
   def postReturnEC2Info() = Action{
-    //    Scala symbol
-    //    http://blog.csdn.net/bobozhengsir/article/details/13023023
+// 此函數，用來回傳queryEC2Info網頁，使用者輸入表單後的頁面呈現
+//    Scala symbol
+//    http://blog.csdn.net/bobozhengsir/article/details/13023023
 
+    // 取得request的內容，並使用userForm剖析及取值
     implicit request => val test = userForm.bindFromRequest().get
-
-
-
       // http://stackoverflow.com/questions/7315327/how-to-check-ec2instance-is-running-or-stop-in-aws-using-java
       //  i-f7e74852
+
+//    DescribeInstancesRequest物件用來放入描述查詢list
       val descrInstanceRequest = new DescribeInstancesRequest()
       //    val javaList = new util.ArrayList[String](5)
       //    javaList.add("i-f7e74852")
@@ -49,22 +51,29 @@ class Query extends Controller {
 
       //    Introduce HowToUse Filter
       //    http://docs.aws.amazon.com/storagegateway/latest/userguide/FindingSnapshotsUsingJava.html
+
+//    創建aws filter，然後放入descrInstanceRequest物件的值內
       import com.amazonaws.services.ec2.model.Filter
       val ec2Filter = new Filter()
       //    ec2Filter.withName("instance-id").withValues("")
       ec2Filter.withName("key-name").withValues(test.keyName)
       descrInstanceRequest.withFilters(ec2Filter)
 
+//    創建一個Role的credential
       val cred =new com.amazonaws.auth.InstanceProfileCredentialsProvider
 
 
 
       //    descrInstanceRequest.setFilters()
+//    創建Ec2 client，用來使用EC2指令
       val ec2ClientTest = new AmazonEC2Client(cred)
+//    告知是要用在哪一個Region
       ec2ClientTest.setEndpoint("ec2.ap-northeast-1.amazonaws.com")
 
+//    生成查詢結果
       val describeInsResult = ec2ClientTest.describeInstances(descrInstanceRequest)
 
+//    取得Reservations內的Instance清單，若公司有買保留Instance，則這邊需要修改
       val reservationsResult = describeInsResult.getReservations()
 
       //    println(reservationsResult.get(0).getInstances.get(0).getInstanceId())        // instance-id
@@ -76,6 +85,7 @@ class Query extends Controller {
 
       var tableContent = ""
 
+//    生成table內容
       for(i <- 0 to reservationsResult.size()-1){
         tableContent = tableContent +"<tr>"
         val tempInstance = reservationsResult.get(i).getInstances.get(0)
@@ -99,8 +109,7 @@ class Query extends Controller {
 
       //    println(ec2ClientTest.describeInstances())
       //    println(" ddd")
-      //    println(ec2ClientTest.describeInstances())
-
+      //    println(ec2ClientTest.describeInstances()
       Ok(views.html.queryEC2Info(play.twirl.api.Html(tableContent)))
   }
 
